f77d4fa77a677cfb12ae30dd9517700e5ff37e7f
cfrazier1978 <66221343+cfrazier1978@users.noreply.github.com>
2021-05-18 17:52:54 -0400

    Fixed PSUB

--- models/psubs.py
+++ models/psubs.py
@@ -3,7 +3,7 @@ from models.updates import *
 partial_state_update_blocks = [
     {
         'policies': {
-            Dam_Policy
+            'Dam_Policy': Dam_Policy
         },    
         'states': {
             'Reservoir_Level': Reservoir_Update,
